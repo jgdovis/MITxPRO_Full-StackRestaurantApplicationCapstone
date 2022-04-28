3f9fd457b5da0b46368134b311237e9adb41d911
Jesse Garcia <68394599+jgdovis@users.noreply.github.com>
2022-04-28 13:57:07 -0500

    Update restaurantList.js

--- components/restaurantList.js
+++ components/restaurantList.js
@@ -56,7 +56,7 @@ if(searchQuery.length > 0){
           top={true}
           style={{ height: 200 }}
           src={
-          `http://localhost:1337`+ res.image.url
+          `http://18.224.252.54`+ res.image.url
           }
         />
         <CardBody>
