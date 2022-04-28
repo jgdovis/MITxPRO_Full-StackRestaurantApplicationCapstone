1ad6db3a3bcfa7565feafd49d1fb35e07db374ab
Jesse Garcia <jegatx79@gmail.com>
2022-04-28 00:35:54 -0500

    update

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    reactStrictMode: true,
+    images: {
+        domains: ['https://yann-gueguenfullstackrestaurantapp.onrender.com/uploads'],
+        formats: ["image/webp"],
+    },
+}
