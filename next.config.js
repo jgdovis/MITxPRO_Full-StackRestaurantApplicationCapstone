8dd184201481ffe321891d8fdbad807104ad20f1
Jesse Garcia <68394599+jgdovis@users.noreply.github.com>
2022-04-28 01:25:37 -0500

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    reactStrictMode: true,
-    images: {
-        domains: ['https://yann-gueguenfullstackrestaurantapp.onrender.com/uploads'],
-        formats: ["image/webp"],
-    },
-}
